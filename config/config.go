10a139d58a8d987cd2a9098cfabbae609d1eda2a
K-shir0 <kakishiro2190394@gmail.com>
2021-05-17 22:39:25 +0900

    feat: redis要のconfig項目の追加

--- config/config.go
+++ config/config.go
@@ -3,7 +3,12 @@ package config
 import "github.com/jinzhu/configor"
 
 type Config struct {
-	Port string `default:"1323"`
+	Port  string `default:"1323"`
+	Redis struct {
+		Password string `default:""`
+		Port     string `default:"6379"`
+		Host     string `default:"127.0.0.1"`
+	}
 }
 
 func New() *Config {
