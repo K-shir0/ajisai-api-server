2a19d8ee3a6492e14ea5846de5c5e3728ee8cd10
K-shir0 <kakishiro2190394@gmail.com>
2021-05-21 22:58:58 +0900

    feat: 気象情報を挿入する処理を実装

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,11 +1,14 @@
 package infrastructure
 
 import (
+	"context"
 	"fmt"
 	"github.com/K-shir0/ajisai-api-server/config"
 	"github.com/K-shir0/ajisai-api-server/domain"
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
+	"time"
 )
 
 type Router struct {
@@ -25,22 +28,44 @@ func Init() {
 	// Ping the primary
 	fmt.Println(db)
 
-	// ルーティング
-	r.e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	// Context
+	ctx := context.Background()
+
+	// ルーティングa
+	r.e.GET("/weathers", func(c echo.Context) error {
+
+		collection := db.Database("test").Collection("hoge")
+
+		cur, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			return err
+		}
+
+		var weathers []bson.M
+		if err = cur.All(ctx, &weathers); err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, weathers)
 	})
 
-	r.e.POST("/", func(c echo.Context) error {
+	r.e.POST("/weathers", func(c echo.Context) error {
 		param := new(domain.Weather)
 		if err := c.Bind(param); err != nil {
 			return err
 		}
 
-		//TODO 日付
+		// set date
+		now := time.Now()
+		param.UpdatedAt, param.CreatedAt = now, now
 
-		//TODO DBに入れる処理
+		// insert DB
+		res, err := db.Database("test").Collection("hoge").InsertOne(ctx, param)
+		if err != nil {
+			return err
+		}
 
-		return c.JSON(http.StatusOK, param)
+		return c.JSON(http.StatusOK, res)
 	})
 
 	// echo server start
