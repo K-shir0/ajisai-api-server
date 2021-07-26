6ddd9ca1310b368d569bc340586abe3230ddb0a1
K-shir0 <kakishiro2190394@gmail.com>
2021-07-26 11:38:29 +0900

    feat: websocketの処理一旦削除した

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,11 +1,9 @@
 package infrastructure
 
 import (
-	"fmt"
 	"github.com/K-shir0/ajisai-api-server/config"
 	"github.com/K-shir0/ajisai-api-server/domain"
 	"github.com/K-shir0/ajisai-api-server/interfaces/database"
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"net/http"
@@ -24,7 +22,7 @@ func Init() {
 
 	r.e.Use(middleware.CORS())
 
-	// コントローラの呼び出し
+	// sqlコントローラの呼び出し
 	db := r.NewSqlHandler()
 
 	wr := database.WeathersRepository{Collection: db.Collection("weather")}
@@ -52,70 +50,9 @@ func Init() {
 			return err
 		}
 
-		message = param
-		flag = true
-
 		return c.JSON(http.StatusOK, res)
 	})
 
-	r.e.GET("/ws", hello)
-
 	// echo server start
 	r.e.Logger.Fatal(r.e.Start(":" + r.config.Port))
-}
-
-var message *domain.Weather
-var flag = false
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
-)
-
-func hello(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer ws.Close()
-
-	for {
-		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-		if err != nil {
-			c.Logger().Error(err)
-		}
-
-		// Read
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			c.Logger().Error(err)
-		}
-		fmt.Printf("%s\n", msg)
-	}
-
-	//websocket.Handler(func(ws *websocket.Conn) {
-	//	defer func(ws *websocket.Conn) {
-	//		err := ws.Close()
-	//		if err != nil {
-	//			c.Logger().Error(err)
-	//		}
-	//	}(ws)
-	//	for {
-	//		// Write
-	//		if flag {
-	//			flag = false
-	//
-	//			err := websocket.JSON.Send(ws, message)
-	//			if err != nil {
-	//				c.Logger().Error(err)
-	//				break
-	//			}
-	//		}
-	//	}
-	//}).ServeHTTP(c.Response(), c.Request())
-	//return nil
 }
