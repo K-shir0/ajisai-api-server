794b37f45e82ca72494c6c3bd39bb3478f9a88d7
K-shir0 <kakishiro2190394@gmail.com>
2021-07-26 12:30:56 +0900

    add 新しいフォーマットに変更

--- domain/weathers.go
+++ domain/weathers.go
@@ -5,15 +5,23 @@ import "time"
 type Weathers []Weather
 
 type Weather struct {
-	SoilTemperature *int      `json:"soilTemperature"`
-	SoilMoisture    *int      `json:"soilMoisture"`
-	WindSpeed       *int      `json:"windSpeed"`
-	WindDirection   string    `json:"windDirection"`
-	Temperature     *int      `json:"temperature"`
-	Pressure        *int      `json:"pressure"`
-	Humidity        *int      `json:"humidity"`
-	Altitude        *int      `json:"altitude"`
-	Rain            *float64  `json:"rain"`
-	CreatedAt       time.Time `json:"createdAt"`
-	UpdatedAt       time.Time `json:"updatedAt"`
+	Id   string    `json:"id"`
+	Time time.Time `json:"time"`
+	Lati float64   `json:"lati,string"`
+	Long float64   `json:"long,string"`
+	Temp float64   `json:"temp,string"`
+	Humi float64   `json:"humi,string"`
+	Baro float64   `json:"baro,string"`
+	Rain float64   `json:"rain,string"`
+	Wind struct {
+		Spee float64 `json:"spee,string"`
+		Dire float64 `json:"dire,string"`
+		Gust float64 `json:"gust,string"`
+	} `json:"wind"`
+	Ligh struct {
+		Dura float64 `json:"dura,string"`
+		Amou float64 `json:"amou,string"`
+	} `json:"ligh"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
