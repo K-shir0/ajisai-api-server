1d4c817baba78ce097edc56805402ce31b705e85
K-shir0 <kakishiro2190394@gmail.com>
2021-07-29 14:51:55 +0900

    add nullを入力したときに0が入らないようにした

--- domain/weathers.go
+++ domain/weathers.go
@@ -5,22 +5,22 @@ import "time"
 type Weathers []Weather
 
 type Weather struct {
-	Id   string    `json:"id"`
-	Time time.Time `json:"time"`
-	Lati float64   `json:"lati,string"`
-	Long float64   `json:"long,string"`
-	Temp float64   `json:"temp,string"`
-	Humi float64   `json:"humi,string"`
-	Baro float64   `json:"baro,string"`
-	Rain float64   `json:"rain,string"`
-	Wind struct {
-		Spee float64 `json:"spee,string"`
-		Dire float64 `json:"dire,string"`
-		Gust float64 `json:"gust,string"`
+	Id   string     `json:"id"`
+	Time *time.Time `json:"time"`
+	Lati *float64   `json:"lati,string"`
+	Long *float64   `json:"long,string"`
+	Temp *float64   `json:"temp,string"`
+	Humi *float64   `json:"humi,string"`
+	Baro *float64   `json:"baro,string"`
+	Rain *float64   `json:"rain,string"`
+	Wind *struct {
+		Spee *float64 `json:"spee,string"`
+		Dire *float64 `json:"dire,string"`
+		Gust *float64 `json:"gust,string"`
 	} `json:"wind"`
-	Ligh struct {
-		Dura float64 `json:"dura,string"`
-		Amou float64 `json:"amou,string"`
+	Ligh *struct {
+		Dura *float64 `json:"dura,string"`
+		Amou *float64 `json:"amou,string"`
 	} `json:"ligh"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
